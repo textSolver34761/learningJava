f1a0897ec42ede4445a148a1039c042cd7056109
Benjamin Pradon <pradonbenjamin@gmail.com>
2018-08-03 12:46:02 +0200

    JPA Repo

--- springboot-data/src/main/java/springboot/topic/Topic.java
+++ springboot-data/src/main/java/springboot/topic/Topic.java
@@ -1,7 +1,12 @@
 package springboot.topic;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 	
+	@Id
 	private String id;
 	private String Name;
 	private String Description;
---
+++ springboot-data/src/main/java/springboot/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package springboot.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository <Topic, String> {
+	//
+}
