ac883337178e047ff41a59ad5ae5471b791ff543
Benjamin Pradon <pradonbenjamin@gmail.com>
2018-08-03 13:27:25 +0200

    CRUD opp

--- springboot-data/src/main/java/springboot/topic/TopicSerices.java
+++ springboot-data/src/main/java/springboot/topic/TopicSerices.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicSerices {
+	
+	@Autowired
+	private TopicRepository topicrepo;
 	
 	private List <Topic> topics = new ArrayList <> (Arrays.asList(
 			new Topic("Spring","Spring Framework","Randdom description"),
@@ -18,30 +22,36 @@ public class TopicSerices {
 			));
 	
 	public List <Topic> getAll(){
+		//return topics;
+		List<Topic> topics = new ArrayList<>();
+		topicrepo.findAll().forEach(topics::add);
 		return topics;
 	}
 	
 	public Topic get(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		//return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicrepo.findById(id);
 	}
 
 	public void add(Topic topic) {
-		topics.add(topic);
+		//topics.add(topic);
+		topicrepo.save(topic);
 		
 	}
 
 	public void update(String id, Topic topic) {
-		for (int i = 0; i < topics.size(); i++) {
+		/*for (int i = 0; i < topics.size(); i++) {
 			Topic t = topics.get(i);
 			if(t.getId().equals(id)) {
 				topics.set(i, topic);
 				return;
 			}
-		}
-		
+		}*/
+		topicrepo.save(topic);
 	}
 
 	public void delete(String id) {
-		topics.removeIf(t -> t.getId().equals(id));
+		//topics.removeIf(t -> t.getId().equals(id));
+		topicrepo.deleteById(id);
 	}
 }
